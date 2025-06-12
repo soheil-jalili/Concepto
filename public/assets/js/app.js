80b57b232b70367043d5894636ed7c096b901034
soheil-jalili <jalilisoheil14@gmail.com>
2025-06-12 15:55:14 -0700

    add hero statistics and fix some bugs in header

---
+++ public/assets/js/app.js
@@ -0,0 +1,27 @@
+// Menu
+const menuBtn = document.querySelector("#btn__menu"),
+  overlay = document.querySelector("#overlay"),
+  closeBtn = document.querySelector("#closeBtn"),
+  menuItems = document.querySelector("#menu__items");
+
+menuBtn.addEventListener("click", () => {
+  menuItems.classList.remove(
+    "-right-1/2",
+    "-right-3/4",
+    "max-[310px]:-right-3/4"
+  );
+
+  menuItems.classList.add("right-0");
+  overlay.classList.add("visible", "opacity-100");
+});
+
+const closeMenu = () => {
+  menuItems.classList.remove("right-0");
+
+  menuItems.classList.add("-right-1/2", "max-[310px]:-right-3/4");
+
+  overlay.classList.remove("visible", "opacity-100");
+};
+
+overlay.addEventListener("click", closeMenu);
+closeBtn.addEventListener("click", closeMenu);
