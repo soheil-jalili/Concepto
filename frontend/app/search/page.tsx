344492faa86cffd5a2627c328759391a6af37ce4
soheil-jalili <jalilisoheil14@gmail.com>
2026-08-19 15:10:53 -0700

    feat(search): implement search page functionality and UI

---
+++ frontend/app/search/page.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/modules/Header/Header";
+import React from "react";
+
+const Search: React.FC = () => {
+  return (
+    <>
+      <Header />
+
+      <section className="container">
+        <p>جستجو</p>
+      </section>
+    </>
+  );
+};
+
+export default Search;
